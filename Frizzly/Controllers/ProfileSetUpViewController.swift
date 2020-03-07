b7a8968b3da792d4f446b8a258c45ad42a1b90c8
pateldevang <devangpatel92@gmail.com>
2020-03-08 03:37:35 +0530

    access code UI done.

--- Frizzly/Controllers/ProfileSetUpViewController.swift
+++ Frizzly/Controllers/ProfileSetUpViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class ProfileSetUpViewController: UIViewController {
 
+    // MARK :- Outlets
+    @IBOutlet weak var code: UITextField!
+    @IBOutlet weak var netact: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Setting up textFields
+        textFieldDelegateSetUp()
+        //EXTENSION: - Hide keyborad
+        hideKeyboardWhenTappedAround()
+        // Hide activity monitor
+        self.netact.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Function for checking newtwork connection
+        checkNewtork(ifError: "Cannot login")
     }
-    */
+    
+    //MARK:- TextField Delegate Method
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+    @IBAction func submit(_ sender: Any) {
+        self.netact.isHidden = false
+        netact.startAnimating()
+    }
+    
+}
 
+
+
+//MARK: - Extensions
+extension ProfileSetUpViewController: UITextFieldDelegate {
+    
+    //Setup textfield delegates
+    func textFieldDelegateSetUp() {
+        code.delegate = self
+    }
 }
