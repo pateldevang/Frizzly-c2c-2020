738e72a50ea42c4add922d43334204a96e1c659c
pateldevang <devangpatel92@gmail.com>
2020-03-07 23:34:13 +0530

    first commit

---
+++ Frizzly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frizzly
+//
+//  Created by Devang Patel on 07/03/20.
+//  Copyright © 2020 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
