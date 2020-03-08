a36aec43d7d2c35195631ee4e93bd1d419cc6042
pateldevang <devangpatel92@gmail.com>
2020-03-08 12:27:50 +0530

    settings page done.

---
+++ Frizzly/Controllers/settingstudentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  settingstudentViewController.swift
+//  Frizzly
+//
+//  Created by Devang Patel on 08/03/20.
+//  Copyright © 2020 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class settingstudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        signOut()
+    }
+    
+}
