3844aedf0998d8c05fcdab578dc275eb3e896740
pateldevang <devangpatel92@gmail.com>
2020-03-08 11:51:26 +0530

    setting view done.

---
+++ Frizzly/Controllers/settingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  settingsViewController.swift
+//  Frizzly
+//
+//  Created by Devang Patel on 08/03/20.
+//  Copyright © 2020 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class settingsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signout(_ sender: Any) {
+        signOut()
+    }
+}
